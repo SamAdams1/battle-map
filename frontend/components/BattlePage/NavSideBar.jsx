ce855c1d002578a6e7b01a8410031aff499a9043
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-08-12 19:37:38 -0400

    save nav bar scroll progress

--- frontend/components/BattlePage/NavSideBar.jsx
+++ frontend/components/BattlePage/NavSideBar.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const NavSideBar = ({ countryList, setCountry, selectedCountry }) => {
   const [sideBarVis, setSideBarVis] = useState(false);
   let lastLetter = "";
+
+  const scrollRef = useRef(0);
+
+  const handleScroll = () => {
+    if (scrollRef.current) {
+      const scrollTop = scrollRef.current.scrollTop;
+      localStorage.setItem("navScroll", scrollTop);
+    }
+  };
+
+  useEffect(() => {
+    if (sideBarVis)
+      scrollRef.current.scrollTop = localStorage.getItem("navScroll");
+  }, [sideBarVis]);
 
   return (
     <div className="absolute left-0 top-24 z-50">
@@ -13,10 +27,15 @@ const NavSideBar = ({ countryList, setCountry, selectedCountry }) => {
         {sideBarVis ? <>Hide</> : <>Show</>} Nav
       </button>
       {sideBarVis && (
-        <div className="fixed bg-slate-100 max-h-[40em] overflow-auto px-2">
-          <a href="#Top" className="fixed bg-slate-100 w-60 top">
+        <div
+          className="fixed bg-slate-100 max-h-[40em] overflow-auto px-2"
+          ref={scrollRef}
+          onScroll={handleScroll}
+        >
+          <a href="#Top" className="fixed bg-slate-100 w-60">
             (Top)
           </a>
+          <span id="Top"></span>
           <div className="mt-7 mb-2">
             {countryList.map((country) => {
               return (
